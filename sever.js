742e383cef05cdbcdc9828cac7f283fff4744bc9
msalves008 <matheusdesousaalves2018@gmail.com>
2020-02-20 17:59:10 -0300

    Back-end

--- sever.js
+++ sever.js
@@ -8,42 +8,62 @@ nunjucks.configure("./",{
     noCache: true,    
 })
 
-//lista de doadores
-const donors = [
-
-    {
-        name: "Kaio Viana",
-        blood: "AB+"
-    },
-    {
-        name: "Matheus de Sousa",
-        blood: "O-"
-    },
-    {
-        name: "Sergio Calura",
-        blood:"A+"
-    },
-    {
-        name:"Fernando Henrique",
-        blood:"O+"
-    }
-]
-
 //CONFIGURANDO ARQUIVOS EXTRAS (Estaticos)
 //Ex: CSS: JS : logo
 server.use(express.static('public'))
 
-//chando index.html
+//habilitar body do formulario
+server.use(express.urlencoded({ extended: true}))
+
+//configurando conexão com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '1498',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
+//chamando index.html
 server.get("/", function(req, res){
+    const donors = []
     return res.render("index.html",{donors})
 })
 
+ //pegando dados do formulário
+ server.post("/", function(req, res){
+   
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    //verificando se os campos foram preenchidos
+    if(name == "" || email == "" || blood == ""){
+        return res.send("Por favor!\n Preencha Todos Os Campos")
+    }
+
+    //colocando valores dentro do banco de dados
+    const query = `INSERT INTO "public"."donors" ("name","email","blood") VALUES ($1, $2, $3)`     
+
+    const valores = [name, email, blood]
+    
+    db.query(query, valores, function(err){
+        //caso der erro
+        if(err) {
+            return res.send("Erro no Banco de Dados.")
+        }
+        //se estiver ok
+        else{
+            return res.redirect("/")   
+        }
+
+    })
+
+})
 
 
-
-
-
- //iniciando o servidor na porta 3000
+//iniciando o servidor na porta 3000
 server.listen(3000, function(){
     console.log("SERVER OK")
 })
